73bfa39b040766d44d5bb1501bcbb3b798ad929b
davidfrdian <dferdian11@gmail.com>
2025-02-13 14:22:49 +0700

    create about and still develop skills section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Lorem, ipsum dolor.</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-briefcase-alt-2 about__icon"></i>
+        <h3 className="about__title">Lorem.</h3>
+        <span className="about__subtitle">Lorem, ipsum dolor.</span>
+      </div>
+
+      <div className="about__box">
+        <i class="bx bx-support about__icon"></i>
+        <h3 className="about__title">Lorem.</h3>
+        <span className="about__subtitle">Lorem, ipsum dolor.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
