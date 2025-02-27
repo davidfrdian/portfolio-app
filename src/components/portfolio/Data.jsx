90a5c76064c9c2bd9fab1b2e4a70e377cf3e1faa
davidfrdian <dferdian11@gmail.com>
2025-02-27 18:37:04 +0700

    add project

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -2,6 +2,9 @@ import Work1 from "../.././assets/work1.png";
 import Work2 from "../.././assets/work2.png";
 import Work3 from "../.././assets/work3.png";
 import Work4 from "../.././assets/work4.png";
+import Work5 from "../.././assets/work5.png";
+import Work7 from "../.././assets/work7.png";
+
 
 export const projectsData = [
   {
@@ -27,6 +30,27 @@ export const projectsData = [
     image: Work4,
     title: "Spider Club",
     category: "website",
+  },
+
+  {
+    id: 5,
+    image: Work5,
+    title: "Cat Breeds",
+    category: "website",
+  },
+
+  {
+    id: 6,
+    image: Work4,
+    title: "Podcast: Representasi dan Tantangan Perempuan di Dunia Politik | FPCI Talks!",
+    category: "other",
+  },
+
+  {
+    id: 7,
+    image: Work7,
+    title: "Teaser Unboxing Foreign Policy #6 Event",
+    category: "other",
   },
 ];
 
